2104e0c5d07924bffdab25ee2faa94b8faa09370
DaweN7 <114224316+DaweN7@users.noreply.github.com>
2024-11-05 19:42:05 +0700

    first commit

---
+++ src/components/InteractScreen.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="screen">
+    <div
+      class="screen__inner"
+      :style="{
+        width: `${
+          ((((920 - 16 * 4) / Math.sqrt(cardsContext.length) - 16) * 3) / 4 +
+            16) *
+          Math.sqrt(cardsContext.length)
+        }px`,
+      }"
+    >
+      <card-flip
+        v-for="(card, index) in cardsContext"
+        :key="index"
+        :ref="`card-${index}`"
+        :imgBackFaceUrl="`images/${card}.png`"
+        :card="{ index, value: card }"
+        :cardsContext="cardsContext"
+        @onFlip="checkRule($event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CardFlip from "./Card.vue";
+export default {
+  props: {
+    cardsContext: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+
+  components: {
+    CardFlip,
+  },
+
+  data() {
+    return {
+      rules: [],
+    };
+  },
+
+  methods: {
+    checkRule(card) {
+      if (this.rules.length === 2) return false;
+
+      this.rules.push(card);
+
+      if (
+        this.rules.length === 2 &&
+        this.rules[0].value === this.rules[1].value
+      ) {
+        //Add disable mode for 2 card which are same
+        this.$refs[`card-${this.rules[0].index}`][0].onEnableDisableMode();
+        this.$refs[`card-${this.rules[1].index}`][0].onEnableDisableMode();
+
+        this.rules = [];
+
+        const disabledElements = document.querySelectorAll(
+          ".screen .card.disabled"
+        );
+
+        if (
+          disabledElements &&
+          disabledElements.length === this.cardsContext.length - 2
+        ) {
+          setTimeout(() => {
+            this.$emit("onFinish");
+          }, 1000);
+        }
+      } else if (
+        this.rules.length === 2 &&
+        this.rules[0].value !== this.rules[1].value
+      ) {
+        setTimeout(() => {
+          //close card
+          this.$refs[`card-${this.rules[0].index}`][0].onFlipBackCard();
+          this.$refs[`card-${this.rules[1].index}`][0].onFlipBackCard();
+          //reset rules
+          this.rules = [];
+        }, 500);
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.screen {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: var(--dark);
+  color: var(--light);
+}
+
+.screen__inner {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2rem auto;
+}
+</style>
